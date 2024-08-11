943b0131b49f479d0d0bae9b0e946e5800f9f8d0
lqdark <lqdark@mail.ru>
2024-08-11 12:27:01 +0500

    T2U3

---
+++ T2U3_pr1.py
@@ -0,0 +1,9 @@
+print('Enter the type of animal' )
+a_type=input()
+print('Enter the age of animal' )
+a_age=input()
+print("Enter the animal's nickname" )
+a_name=input()
+
+print('This is a', a_type, 'named', '"'+a_name+'"' + '.', 'Age:', a_age, 'years old.')
+
